8f9d06dc127f8f9ea0eca1942a2dc3f324b86d3c
camilistarosky <camili.naara@gmail.com>
2025-06-17 06:05:04 -0300

    ajustes

--- desafios/codewars/src/evenOrOdd.java
+++ desafios/codewars/src/evenOrOdd.java
@@ -1,15 +1,16 @@
+
 /*Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-*/
-public class Kata {
-    public static String evenOrOdd(int number) {
+ */
+public class evenOrOdd {
+    public static String checkEvenOrOdd(int number) {
         String result;
 
-        if(number % 0 == 0){
+        if(number % 2 == 0){
             result = "Even";
         } else{
             result = "Odd";
         }
 
-        return "result";
+        return result;
     }
 }
